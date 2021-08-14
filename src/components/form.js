1dc9381176eb8556fb92d821388e9bffbf5b4525
mesoyoung <mesoyoung123@gmail.com>
2021-08-14 23:23:24 +0800

    added rating in form.js

--- src/components/form.js
+++ src/components/form.js
@@ -1,114 +1,101 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './form.css'
 
-class Form extends Component {
+const Form = () => {
 
-    constructor(props) {
-        super(props)
+    const [name, setName] = useState('');
+    const [contacts, setContacts] = useState('');
+    const [contactType, setContactType] = useState('Whatsapp');
+    const [email, setEmail] = useState('');
+    const [rating, setRating] = useState(0);
+    const [feedback, setFeedback] = useState('');
 
-        this.state = {
-            fullName: '',
-            contactInfo: '',
-            contactType: '',
-            email: '',
-            feedback: '',
 
-        }
-    }
-
-    handleFullNameChange = (event) => {
-        this.setState({
-            fullName: event.target.value
-        })
-    }
-
-    handleContactInfoChange = (event) => {
-        this.setState({
-            contactInfo: event.target.value
-        })
-    }
-
-    handleEmailChange = (event) => {
-        this.setState({
-            email: event.target.value
-        })
-    }
-
-    handleFeedbackChange = (event) => {
-        this.setState({
-            feedback: event.target.value
-        })
-    }
-
-    handleContactTypeChange = (event) => {
-        this.setState({
-            contactType: event.target.value
-        })
-    }
-
-    handleSubmit = event => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
         alert(`Contacts submitted successfully! An email containing the details will be sent to you shortly.`)
-        event.preventDefault()
     }
 
-    render() {
-        const { fullName, contactInfo, contactType, email, feedback } = this.state
-        return (
-            <form onSubmit={this.handleSubmit} className='form'>
 
-                <div className = 'header'>Contact Form</div>
+    return (
+        <form onSubmit={handleSubmit} className='form'>
 
-                <div className ='fullName'>
-                    <label>Full Name* </label>
-                    <input
-                        type='text'
-                        value={fullName}
-                        onChange={this.handleFullNameChange}
-                    />
-                </div>
-                
+            <div className='header'>Contact Form</div>
 
-                <div className='contactInfo'>
-                    <label>Mobile No/Telegram ID*</label>
-                    <input
-                        type='text'
-                        value={contactInfo}
-                        onChange={this.handleContactInfoChange}
-                        pattern = "[a-zA-Z][a-zA-Z0-9]{4,31}|[8-9]{1}[0-9]{7}"
-                    />
-                </div>
+            <div className='fullName'>
+                <label>Full Name* </label>
+                <input
+                    type='text'
+                    onChange={(event) => {
+                        setName(event.target.value);
+                    }}
+                />
+            </div>
 
-                <div className='contactType'>
-                    <label>Preferred Contact Type</label>
-                    <select value={contactType} onChange={this.handleContactTypeChange}>
-                        <option value='whatsapp'>Whatsapp</option>
-                        <option value='telegram'>Telegram</option>
-                    </select>
-                </div>
 
-                <div>
-                    <label className='email'>Email*</label>
-                    <input
-                        type='email'
-                        placeholder='(Meetup details will be sent to this email)'
-                        value={email}
-                        onChange={this.handleEmailChange}
-                    />
-                </div>
+            <div className='contactInfo'>
+                <label>Mobile No/Telegram ID*</label>
+                <input
+                    type='text'
+                    onChange={(event) => {
+                        setContacts(event.target.value);
+                    }}
+                    pattern="[a-zA-Z][a-zA-Z0-9]{4,31}|[8-9]{1}[0-9]{7}"
+                />
+            </div>
 
-                <div className='feedback'>
-                    <label>Feedback</label>
-                    <textarea
-                        value={feedback}
-                        onChange={this.handleFeedbackChange}
-                        placeholder = '(Optional)'
-                    />
-                </div>
+            <div className='contactType'>
+                <label>Preferred Contact Type</label>
+                <select
+                    onChange={(event) => {
+                        setContactType(event.target.value);
+                    }}>
+                    <option value='Whatsapp'>Whatsapp</option>
+                    <option value='telegram'>Telegram</option>
+                </select>
+            </div>
 
-                <button type='submit'>Submit</button>
-            </form>
-        );
-    };
+            <div>
+                <label className='email'>Email*</label>
+                <input
+                    type='email'
+                    placeholder='(Meetup details will be sent to this email)'
+                    onChange={(event) => {
+                        setEmail(event.target.value);
+                    }}
+                />
+            </div>
+
+
+            <div className='sliderContainer'>
+                <label>Rate our website!</label>
+                <select
+                    onChange={(event) => {
+                        setRating(event.target.value);
+                    }}>
+                    <option value='0' selected>--</option>
+                    <option value='1'>1/5</option>
+                    <option value='2'>2/5</option>
+                    <option value='3'>3/5</option>
+                    <option value='4'>4/5</option>
+                    <option value='5'>5/5</option>
+                </select>
+            </div>
+
+            <div className='feedback'>
+                <label>Feedback</label>
+                <textarea
+                    onChange={(event) => {
+                        setFeedback(event.target.value);
+                    }}
+                    placeholder='(Optional)'
+                />
+            </div>
+
+            <button type='submit'>Submit</button>
+        </form>
+    );
 };
+
 
 export default Form;
